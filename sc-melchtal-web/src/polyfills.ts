f9ff3eaa38fb7165bd75da02cb8d37c010332110
Stefan Frank <ffets82@gmail.com>
2019-01-15 20:43:23 +0100

    Angular 7 Upgrade.

--- sc-melchtal-web/src/polyfills.ts
+++ sc-melchtal-web/src/polyfills.ts
@@ -35,7 +35,7 @@ import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
